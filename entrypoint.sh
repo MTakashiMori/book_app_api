ff6c06e0a0be0ddc0562a93e0db64c0d959cf06d
mtakashimori <mtakashimori@gmail.com>
2021-02-18 01:03:09 -0300

    update project

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,31 @@
 #!/bin/sh
 
-
 echo "************** Installing laravel dependencies **************"
-composer install --verbose
+    composer clear-cache
+    composer install --verbose
 
-php artisan config:clear
+    php artisan config:clear
 
-if [ -e .env ]
-then
-    echo "************** Refreshing database **************"
-    #  php artisan migrate:fresh --seed
-else
-    echo "************** Copiyng .env **************"
+echo "************** Cleaning Files **************"
+    rm -rf storage/uploads
+
+echo "************** Creating simbolic link to storage **************"
+    php artisan storage:link
+
+echo "************** Creating .env **************"
+    rm -f -- .env
     cp .env.example .env
-    # php artisan migrate:fresh --seed
-fi
+    # IP_ADDR=$(ifconfig en0 | grep inet | grep -v inet6 | awk '{print $2}')
+
+    # sed -i -e "s|DB_HOST=127.0.0.1|DB_HOST=$IP_ADDR|g" .env
+    rm -f -- .env-e
+
+echo "************** Refreshing database **************"
+    php artisan migrate:fresh --seed
 
 echo "************** Generating key and refreshing  **************"
-php artisan key:generate
-php artisan config:cache
+    php artisan key:generate
+    php artisan config:cache
 
 echo "************** Your application is ready to use **************"
 exec "$@"
